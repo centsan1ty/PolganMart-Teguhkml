de62a5aa4bca98c2cc9ec6cc1fce8be3fdef829e
Teguhkml <kakoonaf@gmail.com>
2025-09-22 21:51:08 +0700

    Logika Pembelian

--- PolganMart-Teguhkml.php
+++ PolganMart-Teguhkml.php
@@ -3,3 +3,17 @@ $nama_barang = ["Indomie Goreng", "Aqua 600ml", "Chitato", "Beras 5kg", "Telur 1
 $harga_barang = [3500, 5000, 12000, 60000, 28000];
 
 echo "--POLGAN MART--\n\n";
+
+$beli = [];
+$jumlah = [];
+$total = [];
+$grandtotal = 0;
+
+for ($i = 0; $i < 5; $i++) {
+    $index = rand(0, count($nama_barang) - 1);  // pilih barang random
+    $qty   = rand(1, 5);                        // jumlah random
+
+    $beli[]   = $nama_barang[$index];
+    $jumlah[] = $qty;
+    $total[]  = $harga_barang[$index] * $qty;
+}
